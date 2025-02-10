4fe2eaeb43e08acb62bf43959986b9752c30399f
Administrator <Administrator@i7E-17>
2025-02-10 17:44:56 +0900

    0210

--- src/p0210/review/Exam.java
+++ src/p0210/review/Exam.java
@@ -9,6 +9,8 @@ public class Exam {
 		Exam e2 = new Exam();
 		e2.name = "승현";
 		System.out.print(e1.equals(e2));
+		//object에 있는 equals는 값이 아닌 해쉬코드를 비교한다
+		//String 에 있는 equals는 값을 비교한다 , object에 있는 equals를 오버라이딩
 		
 		
 		
